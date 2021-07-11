65c9bf5fb3e5141e8b69ffcc85e93fe27b86dccc
rafvel17 <rafvel17@ya.ru>
2021-07-11 14:33:08 +0400

    Extract students table from DB via PySpark

--- src/main.py
+++ src/main.py
@@ -1,1 +1,19 @@
-print("Hello, world!")
+from pyspark.sql import SparkSession
+
+DB_URL = "jdbc:postgresql:postgres"
+STUDENTS_TABLE = "university.students"
+
+DB_PROPS = {
+    "user": "postgres",
+    "password": "12345",
+    "driver": "org.postgresql.Driver",
+}
+
+spark = SparkSession \
+    .builder \
+    .appName("my_first_etl") \
+    .config("spark.jars", "../jars/postgresql-42.2.23.jar") \
+    .getOrCreate()
+
+students_df = spark.read.jdbc(url=DB_URL, table=STUDENTS_TABLE, properties=DB_PROPS)
+students_df.show()
